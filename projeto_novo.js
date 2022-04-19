67b0b6a662a1970109e233896da840976494d06d
rafael-alves-nascimento <91340095+rafael-alves-nascimento@users.noreply.github.com>
2022-04-19 16:54:05 -0300

    Add files via upload

---
+++ projeto_novo.js
@@ -0,0 +1,73 @@
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'senai',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB conexão estabelecida.');
+    else
+        console.log('DB conexão falhou \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+
+app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
+
+// metodo get
+app.use(express.json())
+app.get('/', function (req, res) {
+    res.status(200)
+    let dados = 'SELECT * FROM alunos_info'
+    mysqlConnection.query(dados, function(err, result){
+      if(err) throw err
+      console.log(result)
+      res.json(result)
+    })       
+})
+
+app.delete('/:matricula', function (req, res) {
+    var index = req.params.matricula
+    var dados = `delete from alunos_info where matricula = ${index}`
+    mysqlConnection.query(dados, function(err, result){
+      if(err) throw err
+      console.log(result)
+      res.json(result)
+    })   
+    res.status(200)
+    res.send(`O dado com id:${index} foi deletado`)
+  })
+
+//Post  
+app.post('/POST', (req, res) => {
+    let emp = req.body;
+    var sql = "SET @Matricula = ?;SET @CPF = ?;SET @Nome = ?;SET @idade = ?; \
+    CALL ADDouEditar(@Matricula,@CPF,@Nome,@idade);";
+    mysqlConnection.query(sql, [emp.Matricula, emp.CPF, emp.Nome, emp.idade], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted alunos_info id : '+element[0].Matricula);
+            });
+        else
+            console.log(err);
+    })
+});
+
+//Update  
+app.put('/PUT', (req, res) => {
+    let emp = req.body;
+    var sql = "SET @Matricula = ?;SET @CPF = ?;SET @Nome = ?;SET @idade = ?; \
+    CALL ADDouEditar(@Matricula,@CPF,@Nome,@idade);";
+    mysqlConnection.query(sql, [emp.Matricula, emp.CPF, emp.Nome, emp.idade], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    })
+});
